ce864b322e469996485943537533adcbc061362c
Andreas Møller <45060997+foenssmoeller@users.noreply.github.com>
2022-10-28 10:35:47 +0200

    Update JointlyObject.py
    Added plotting functionality

--- src/JOINTLY/JointlyObject.py
+++ src/JOINTLY/JointlyObject.py
@@ -1,5 +1,6 @@
-
-
+import scanpy as sc
+import matplotlib.pyplot as plt
+import numpy as np
 class JointlyObject:
     """
     Container to store Jointly compatible data
@@ -42,3 +43,37 @@ class JointlyObject:
 
     def ExportParams(self):
         pass
+
+
+    def ComputeEmbedding(self):
+        """Make a joint embedding of the merged dataset"""
+        sc.pp.neighbors(self.anndata , n_neighbors = 20, metric = 'cosine', key_added = 'Jointly_neighbors', use_rep = 'X_Jointly' )
+        sc.tl.umap(self.anndata, neighbors_key = 'Jointly_neighbors')
+        self.anndata.obsm['UMAP_Jointly'] = self.anndata.obsm['X_umap']
+
+
+    def ComputeEmbeddingIndividual(self):
+        """Make a embeddings per dataset"""
+        #TODO: make tests to see of ComputeEmbedding has been run
+        for ds in range(self.n_datasets):
+            sc.pp.neighbors(self.adata_list[ds] , n_neighbors = 20, metric = 'cosine', key_added = 'Jointly_neighbors', use_rep = 'X_Jointly' )
+            sc.tl.umap(self.adata_list[ds], neighbors_key = 'Jointly_neighbors')
+            self.adata_list[ds].obsm['UMAP_Jointly'] = self.adata_list[ds].obsm['X_umap']
+
+    def PlotSharedEmbedding(self, cell_key = 'celltype', batch_key = 'Jointly_batch', cmap_celltype = 'tab20', cmap_batch = sc.pl.palettes.default_20, save_key = None):
+        """Plot shared embeddings """
+        #TODO: make tests to see of ComputeEmbedding has been run
+        sc.pl.embedding(self.anndata, basis = 'UMAP_Jointly', color = cell_key, size = 30, add_outline=True, frameon = False, palette = cmap_celltype, save = save_key+'_'+'Celltype.pdf' if type(save_key) is str else False)
+        sc.pl.embedding(self.anndata, basis = 'UMAP_Jointly', color = batch_key,  size = 20, add_outline=True,  frameon = False, palette = cmap_batch, save = save_key+'_'+'Batch.pdf' if type(save_key) is str else False)
+
+    def PlotSharedEmbedding_split(self, batch_key, celltype_key, size=60, frameon=False, legend_loc=None):
+        """Plot split view of shared embedding """
+        #TODO: make tests to see of ComputeEmbedding has been run
+        fig, ax = plt.subplots(1, self.n_datasets, figsize=(5*self.n_datasets, 5))
+        for idx, batch in enumerate(list(set(self.anndata.obs[batch_key]))):
+            tmp = self.anndata.copy()
+            tmp.obs.loc[tmp.obs[batch_key] != batch, celltype_key] = np.NaN
+            tmp1 = tmp[tmp.obs[batch_key] != batch]
+            tmp2 = tmp[tmp.obs[batch_key] == batch]
+            sc.pl.embedding(tmp1, basis = 'UMAP_Jointly', color = celltype_key, size = size*2, add_outline=True, frameon = frameon,ax = ax[idx], show = False, legend_loc = None, title = batch)
+            sc.pl.embedding(tmp2, basis = 'UMAP_Jointly', color = celltype_key, size = size, add_outline=True, frameon = frameon, ax = ax[idx], show = False, title = batch, legend_loc = None if self.n_datasets != idx+1 else 'right margin')
